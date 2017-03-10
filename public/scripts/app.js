b05355b4f4d019a1c2dc74a789dea04866a8856d
Gustavo Dominguez <gdviso@hotmail.com>
2017-03-10 12:15:24 -0500

    we added the lat and long to google maps

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,6 +7,7 @@ var indeedApp = {};
 indeedApp.apiKey = '1211867702868069';
 indeedApp.apiUrl = 'http://api.indeed.com/ads/apisearch';
 indeedApp.googleKey = 'AIzaSyBTN4GtBR709ug6SMg-Sbr55JZvv5ctXys';
+indeedApp.googleSearch = "https://www.google.ca/maps/search/";
 
 // // Collect user input
 // indeedApp.collectInfo = function() {
@@ -63,21 +64,29 @@ indeedApp.displayInfo = function (jobs) {
         var shortDes = job.snippet;
         var datePosted = job.formattedRelativeTime;
         var applyUrl = job.url;
-        indeedApp.lat = job.latitude;
-        indeedApp.long = job.longitude;
         //generate html
         var jobTitleEl = $('<h3>').addClass('jobTitle').html('' + jobTitle);
         var compLoc = $('<h4>').addClass('location').html(company + ' - ' + city + ', ' + state);
         var shortDesEl = $('<p>').addClass('shortDes').html('' + shortDes);
         var dateEl = $('<p>').addClass('date').html('' + datePosted);
         var apply = $('<a>').text("Apply Now!").addClass('seeBtn').attr('href', applyUrl);
-        var showMap = $('<button>').text("Show Map").addClass('showMapBtn');
+        var showMap = $('<button>').text("Show Map").addClass('showMapBtn').attr('data-lat', job.latitude).attr('data-lon', job.longitude);
         //display on html
         $('.results').append(dateEl, jobTitleEl, compLoc, shortDesEl, apply, showMap);
     });
 
     $('.showMapBtn').on('click', function () {
-        // initMap();
+        var lat = $(this).data('lat');
+        var lng = $(this).data('lon');
+        var jobLocation = { lat: lat, lng: lng };
+
+        if (window.marker) {
+            window.marker.setMap(null);
+        }
+        window.marker = new google.maps.Marker({
+            position: jobLocation,
+            map: map
+        });
     });
 };
 
@@ -95,16 +104,18 @@ indeedApp.events = function () {
     });
 };
 
-// indeedApp.initMap = function() {
-// Create a map object and specify the DOM element for display.
-
 var map;
 function initMap() {
-    console.log("??working");
+    var uluru = { lat: -25.363, lng: 131.044 };
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
+        zoom: 8,
+        gestureHandling: 'none'
     });
+    // var marker = new google.maps.Marker({
+    //   position: uluru,
+    //   map: map
+    // });
 }
 // }
 
